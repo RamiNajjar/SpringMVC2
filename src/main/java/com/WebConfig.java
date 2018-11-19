73eaf415bfae93b9a175c455e4ea0765beb73a94
RamiNajjar <RamiNajjar@users.noreply.github.com>
2018-11-19 12:13:25 +0200

    added abser sho

--- src/main/java/com/WebConfig.java
+++ src/main/java/com/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
         resolver.setPrefix("/WEB-INF/views/");
         resolver.setSuffix(".jsp");
         resolver.setExposeContextBeansAsAttributes(true);
-        System.out.println("india");
+        System.err.print("x");
         return resolver;
     }
 
